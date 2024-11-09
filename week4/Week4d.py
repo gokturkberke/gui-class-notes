7686a6342a7feff72572c1fe5c49d907aae46e39
gokturkberke <gokturkberke1907@gmail.com>
2024-11-09 21:52:16 +0300

    d and e also done

--- week4/Week4d.py
+++ week4/Week4d.py
@@ -7,28 +7,28 @@ win.title("Week 4")
 win.geometry("300x200+400+100")
 win.iconbitmap("python.ico")
 
-menubar = tk.Menu(win)
-win.configure(menu=menubar)
+menubar = tk.Menu(win) #menu cubugunu olusturur
+win.configure(menu=menubar) #pencereyi menu cubuguyla iliskilendirir
 
-file_menu = tk.Menu(menubar, tearoff=False)
+file_menu = tk.Menu(menubar, tearoff=False) #file menusunu olusturur
 file_menu.add_command(label="New", accelerator="Ctrl+N", underline=0)
 file_menu.add_command(label="Open...", underline=0)
 file_menu.add_separator()
 
 # Create a sub menu
-sub_menu = tk.Menu(file_menu, tearoff=False)
-sub_menu.add_command(label="Zoom in")
-sub_menu.add_command(label="Zoom out")
+sub_menu = tk.Menu(file_menu, tearoff=False) #Editor adi altinda verilen bir alt menu olsturur bu alt menu file_menu icinde yer alacak ve menu ogeleri barindiracak
+sub_menu.add_command(label="Zoom in") #zoom in komutunu ekler alt menuye kullaniciya yakinlastirma secenegi sunar
+sub_menu.add_command(label="Zoom out") #ayin sekil uzaklastirmali
 
 # Add the sub menu under "Editor" (File > Editor)
-file_menu.add_cascade(label="Editor", menu=sub_menu)
+file_menu.add_cascade(label="Editor", menu=sub_menu) #file menusune editor adli bir baslik ekler daha once olusturdugumuz sub_menu alt menusunu yerlestirir
 file_menu.add_separator()
-file_menu.add_command(label="Exit", command=win.destroy, underline=1)
+file_menu.add_command(label="Exit", command=win.destroy, underline=1) #exit komutunu ekler
 
-help_menu = tk.Menu(menubar, tearoff=False)
+help_menu = tk.Menu(menubar, tearoff=False) # help menusu olusturur
 help_menu.add_command(label="About...")
 
-menubar.add_cascade(label="File", menu=file_menu, underline=0)
-menubar.add_cascade(label="Help", menu=help_menu)
+menubar.add_cascade(label="File", menu=file_menu, underline=0) #file menusunu menu cubuguna ekler
+menubar.add_cascade(label="Help", menu=help_menu) #help menusunu menu cubuguna ekler
 
 win.mainloop()
--- week4/Week4e.py
+++ week4/Week4e.py
@@ -2,15 +2,15 @@
 
 import tkinter as tk
 
-def print_state():
-    print(check_state.get())
+def print_state(): #kullanici tarafindan checkbutton durumunu (secili mi secili degil mi ) yazdirir
+    print(check_state.get()) 
 
 win = tk.Tk()
 win.title("Week 4")
 win.geometry("300x200+400+100")
 win.iconbitmap("python.ico")
 check_state = tk.BooleanVar()  # variable for the check button state
-
+#check butonunun secili olup olmadigini saklayacak degisken
 menubar = tk.Menu(win)
 win.configure(menu=menubar)
 
@@ -27,14 +27,14 @@ file_menu.add_cascade(label="Editor", menu=sub_menu)
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=win.destroy, underline=1)
 
-help_menu = tk.Menu(menubar, tearoff=False)
+help_menu = tk.Menu(menubar, tearoff=False) #help_menu menusune bir checkbutton ekler
 # Add a check button
 help_menu.add_checkbutton(label="Send anonymous stats", onvalue=True, offvalue=False,
                           variable=check_state, command=print_state)
 help_menu.add_separator()
-help_menu.add_command(label="About...")
+help_menu.add_command(label="About...") #hakkinda secenegi help_menuya ekelr
 
 menubar.add_cascade(label="File", menu=file_menu, underline=0)
 menubar.add_cascade(label="Help", menu=help_menu)
-
+#menuleri menubar a ekleme kodlari 
 win.mainloop()
